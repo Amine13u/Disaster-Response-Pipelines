d7218c2e04d137e7d83d042ada67dbf626c1e7e5
root <aminemannai13@gmail.com>
2020-06-20 17:56:47 +0000

    Update the process_data.py file

--- data/process_data.py
+++ data/process_data.py
@@ -2,15 +2,76 @@ import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    This function Load and merges the datasets
+    
+    Inputs : messages_filepath (str) : Filepath for csv file containing messages dataset 
+             categories_filepath (str) : Filepath for csv file containing categories dataset
+    
+    Output : df (dataframe) : Dataframe containing merged content of messages & categories datasets
+    '''
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge datasets
+    df = messages.merge(categories, on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    This function cleans the dataset
+    
+    Input : df (dataframe) : Dataframe containing merged content from messages and categories datasets
+    
+    Output : df (dataframe) : A clean version of the input df
+    '''
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    
+    # use this row to extract a list of new column names for categories
+    category_colnames = list(row.apply(lambda x: x[:-2]))
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+    
+    # convert column from string to numeric
+    categories[column] = pd.to_numeric(categories[column])
+    
+    # drop the original categories column from `df`
+    df.drop('categories', inplace=True, axis=1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    This function saves the dataset to a SQLite database
+    
+    Inputs : df (dataframe) : Dataframe containing a clean version of the merged content of messages & categories datasets
+            database_filename (string) : Path to SQLite database destination 
+            
+    Output : None         
+    '''
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('MessagesCategories', engine, index=False)
 
 
 def main():
